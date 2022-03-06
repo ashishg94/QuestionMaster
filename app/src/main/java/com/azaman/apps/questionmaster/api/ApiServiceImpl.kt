e41aa36bbd684ece90116d0658b7532bcb415119
ashishg94 <ashishkumarg14@gmail.com>
2022-03-06 16:59:13 +0530

    authentication in progress

--- app/src/main/java/com/azaman/apps/questionmaster/api/ApiServiceImpl.kt
+++ app/src/main/java/com/azaman/apps/questionmaster/api/ApiServiceImpl.kt
@@ -3,6 +3,7 @@ package com.azaman.apps.questionmaster.api
 import com.azaman.apps.questionmaster.model.Question
 import javax.inject.Inject
 
+//TODO Need to implement authentication
 class ApiServiceImpl @Inject constructor(private val apiService: ApiService){
     suspend fun getQuestionMasterItem() : List<Question> =
         apiService.getQuestionMasterItem()
